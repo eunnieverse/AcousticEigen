5f5a11c65e056cfa4911b139884f7ae67a7de60f
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-06-23 19:08:19 -0400

    removed excess repetitions

--- sample/3D_water_nonlocal_not_working_Anshuman/Fc.m
+++
@@ -1,8 +0,0 @@
-function [ out ] =  Fc(mu,om)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-%def Fc(mu,om):  # for circular tube with the radius rw
-cstphys3;
-    out= 1 - 2./rw*(1j*om/mu).^(-0.5) .*besselj(1,rw*(1j*om./mu).^(0.5))./besselj(0,rw*(1j*om./mu).^(0.5));
-end
-
--- sample/3D_water_nonlocal_not_working_Anshuman/alpha.m
+++
@@ -1,7 +0,0 @@
-function [ out ] = alpha(n,a)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-out= (n+1.0/2) * pi/a;
-end
-
