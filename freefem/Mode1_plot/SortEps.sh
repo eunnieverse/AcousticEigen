527f61337c2907661fbb5c302daaf39a1cad68fc
Yoonkyung Eunnie Lee <eunnieverse@gmail.com>
2015-08-20 09:52:07 -0400

    cleaning up

--- freefem/Mode1_plot/SortEps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for dirname in Re_vxjr Re_vyjr Im_vxjr Im_vyjr Re_vxjc Re_vyjc Im_vxjc Im_vyjc Mag_vxjr Mag_vyjr Mag_vxjc Mag_vyjc Mag_vjr Mag_vjc Mag_Pjr Mag_Phc Mag_Tjr Mag_Tjc; 
-do
-  mkdir ${dirname}; 
-  mv ${dirname}*.eps ${dirname}/.
-done;
